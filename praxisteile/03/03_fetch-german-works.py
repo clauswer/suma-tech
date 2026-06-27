f7d52c2b3b9d7efceb583187cd4888b372863c15
Sascha Szott <szott@gmx.de>
2026-06-27 12:48:05 +0200

    Unterstützung von Multi-Autor-Downloads

--- praxisteile/03/03_fetch-german-works.py
+++ praxisteile/03/03_fetch-german-works.py
@@ -17,8 +17,8 @@ def download_file(url, filename):
     response = requests.get(url, timeout=60, stream=True)
     response.encoding = 'utf-8'
     if response.status_code == requests.codes.ok:
-        with open(filename, "w", encoding="utf-8") as file:
-            file.write(response.text)
+        with open(filename, "wb") as file:
+            file.write(response.content)
         print(f"Datei erfolgreich heruntergeladen: {filename}")
         return True
 
@@ -26,21 +26,29 @@ def download_file(url, filename):
     return False
 
 if __name__ == "__main__":
-    base_dir = "german"
+    base_dir = "german-works"
     if not os.path.exists(base_dir):
         os.mkdir(base_dir)
-    input_file = "Goethe-ebooks.txt"
 
-    num_of_downloads = 0
-    with open(input_file, "r", encoding="utf-8") as file:
-        for base_url in file:
-            match = re.search(r"/ebooks/(\d+)$", base_url)
-            if match:
-                ebook_id = match.group(1)
-                url = f"{base_url.rstrip()}.txt.utf-8"
-                print(f"Herunterladen von {url}")
-                file = os.path.join(base_dir, f"{ebook_id}.txt")
-                if download_file(url, file):
-                    num_of_downloads += 1
+    # URLs aus Textdateien im Verzeichnis german-work-urls einlesen
+    base_dir_urls = "german-work-urls"
+    for file_name in os.listdir(base_dir_urls):
+        if file_name.endswith("-ebooks.txt"):
+            input_file = os.path.join(base_dir_urls, file_name)
+            author_name = file_name.replace("-ebooks.txt", "")
+            if not os.path.exists(os.path.join(base_dir, author_name)):
+                os.mkdir(os.path.join(base_dir, author_name))
 
-    print(f"{num_of_downloads} Dateien erfolgreich heruntergeladen.")
+            num_of_downloads = 0
+            with open(input_file, "r", encoding="utf-8") as file:
+                for base_url in file:
+                    match = re.search(r"/ebooks/(\d+)$", base_url)
+                    if match:
+                        ebook_id = match.group(1)
+                        url = f"{base_url.rstrip()}.txt.utf-8"
+                        print(f"Herunterladen von {url}")
+                        file = os.path.join(base_dir, author_name.lower(), f"{ebook_id}.txt")
+                        if download_file(url, file):
+                            num_of_downloads += 1
+
+            print(f"{num_of_downloads} Dateien erfolgreich für Autor {author_name} heruntergeladen.")
